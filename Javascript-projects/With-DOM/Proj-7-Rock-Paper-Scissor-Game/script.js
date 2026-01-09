d6f7591cd8c5ea15cab79f6a4f2038f304888fc9
amanhaidry <amanhaidry9480@gmail.com>
2026-01-09 11:10:11 +0530

    Updated javascript project rock paper scissor : Rearrange javascript code to better code understanding like all dom elements organise together

--- Javascript-projects/With-DOM/Proj-7-Rock-Paper-Scissor-Game/script.js
+++ Javascript-projects/With-DOM/Proj-7-Rock-Paper-Scissor-Game/script.js
@@ -1,20 +1,49 @@
-const options = ["Rock", "Paper", "Scissors"];
+// DOM Elements
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const optionsContainer = document.querySelector(".options-container");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const resetGameBtn = document.getElementById("reset-game-btn");
 
-function getRandomComputerResult() {
-  const randomIndex = Math.floor(Math.random() * options.length);
-  return options[randomIndex];
-}
+// ---Game logic---
 
-function hasPlayerWonTheRound(playerChoice, computerChoice) {
-  return (
-    (playerChoice === "Rock" && computerChoice === "Scissors") ||
-    (playerChoice === "Scissors" && computerChoice === "Paper") ||
-    (playerChoice === "Paper" && computerChoice === "Rock")
-  );
-}
-
+// Variables
+const options = ["Rock", "Paper", "Scissors"]; //possible choices
 let playerScore = 0;
 let computerScore = 0;
+
+// Event Listeners
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
+
+// Functions
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
+}
 
 function getRoundResults(userOption) {
   const computerResult = getRandomComputerResult();
@@ -30,27 +59,22 @@ function getRoundResults(userOption) {
   }
 }
 
-const playerScoreSpanElement = document.getElementById("player-score");
-const computerScoreSpanElement = document.getElementById("computer-score");
-const roundResultsMsg = document.getElementById("results-msg");
-const winnerMsgElement = document.getElementById("winner-msg");
-const optionsContainer = document.querySelector(".options-container");
-const resetGameBtn = document.getElementById("reset-game-btn");
-
-function showResults(userOption) {
-  roundResultsMsg.innerText = getRoundResults(userOption);
-  computerScoreSpanElement.innerText = computerScore;
-  playerScoreSpanElement.innerText = playerScore;
-
-  if (playerScore === 3 || computerScore === 3) {
-    winnerMsgElement.innerText = `${
-      playerScore === 3 ? "Player" : "Computer"
-    } has won the game!`;
-
-    resetGameBtn.style.display = "block";
-    optionsContainer.style.display = "none";
-  }
+function getRandomComputerResult() {
+  const randomIndex = Math.floor(Math.random() * options.length);
+  return options[randomIndex];
 }
+
+function hasPlayerWonTheRound(playerChoice, computerChoice) {
+  return (
+    (playerChoice === "Rock" && computerChoice === "Scissors") ||
+    (playerChoice === "Scissors" && computerChoice === "Paper") ||
+    (playerChoice === "Paper" && computerChoice === "Rock")
+  );
+}
+
+// Reset Game
+resetGameBtn.addEventListener("click", resetGame);
+
 function resetGame() {
   playerScore = 0;
   computerScore = 0;
@@ -64,21 +88,3 @@ function resetGame() {
   winnerMsgElement.innerText = "";
   roundResultsMsg.innerText = "";
 }
-
-resetGameBtn.addEventListener("click", resetGame);
-
-const rockBtn = document.getElementById("rock-btn");
-const paperBtn = document.getElementById("paper-btn");
-const scissorsBtn = document.getElementById("scissors-btn");
-
-rockBtn.addEventListener("click", function () {
-  showResults("Rock");
-});
-
-paperBtn.addEventListener("click", function () {
-  showResults("Paper");
-});
-
-scissorsBtn.addEventListener("click", function () {
-  showResults("Scissors");
-});

3b03ee3b0b722f493cef9da5afc9f293ba9b9689
amanhaidry <amanhaidry9480@gmail.com>
2026-02-06 23:38:41 +0530

    Added reusable footer

---
+++ React-projects/Proj-2-Reusable-Footer/index.jsx
@@ -0,0 +1,43 @@
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <ul>
+          <li>
+            <a href="#">Fitness Dashboard</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Watch Videos</a>
+          </li>
+          <li>
+            <a href="#">Discord</a>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Terms & Conditions</a>
+          </li>
+        </ul>
+      </div>
+
+      <p className="footer-copy">
+        © 2026 Fitness Dashboard. All Rights Reserved.
+      </p>
+
+      <div className="footer-icons">
+        <a href="#">🎮</a>
+        <a href="#">🛼</a>
+        <a href="#">💻</a>
+        <a href="#">🏀</a>
+      </div>
+    </footer>
+  );
+};

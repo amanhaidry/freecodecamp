af20d0007f92e29aa426a068874ea1e7dc4a1ac6
amanhaidry <amanhaidry9480@gmail.com>
2026-03-11 06:39:06 +0530

    continous working on python project polygon area calculator

--- Certification-Projects/Python/03_Polygon_Area_Calculator.py
+++ Certification-Projects/Python/03_Polygon_Area_Calculator.py
@@ -1,3 +1,5 @@
+import math
+
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
@@ -16,7 +18,38 @@ class Rectangle:
         return 2 * (self.width + self.height)
 
     def get_diagonal(self):
-        return (self.width ** 2 + self.height ** 2) ** 0.5
+        return math.sqrt(self.width ** 2 + self.height ** 2)
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        picture = ""
+        for _ in range(self.height):
+            picture += "*" * self.width + "\n"
+        return picture
+
+    def get_amount_inside(self, shape):
+        return (self.width // shape.width) * (self.height // shape.height)
 
     def __str__(self):
         return f"Rectangle(width={self.width}, height={self.height})"
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_width(self, side):
+        self.width = side
+        self.height = side
+
+    def set_height(self, side):
+        self.width = side
+        self.height = side
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def __str__(self):
+        return f"Square(side={self.width})"

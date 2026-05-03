67cffd7e41ba978f08111797eb238beff20baef7
amanhaidry <amanhaidry9480@gmail.com>
2026-05-03 16:34:23 +0530

    Python Project "polygon_area_calculator": Refactor Square class and add main execution block for testing Rectangle and Square functionalities

--- Certification-Projects/Python/03_Polygon_Area_Calculator.py
+++ Certification-Projects/Python/03_Polygon_Area_Calculator.py
@@ -53,3 +53,22 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"Square(side={self.width})"
+    
+if __name__ == "__main__":
+    rect = Rectangle(10, 5)
+    print(rect.get_area())
+    rect.set_height(3)
+    print(rect.get_perimeter())
+    print(rect)
+    print(rect.get_picture())
+
+    sq = Square(9)
+    print(sq.get_area())
+    sq.set_side(4)
+    print(sq.get_diagonal())
+    print(sq)
+    print(sq.get_picture())
+
+    rect.set_height(8)
+    rect.set_width(16)
+    print(rect.get_amount_inside(sq))
